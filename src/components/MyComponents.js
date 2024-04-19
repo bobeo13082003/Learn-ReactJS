d47608eb9c925de7cfd6cf62a8d797a3c1842ddb
bobeo13082003 <phamichbo13082003@gmail.com>
2024-04-19 09:48:15 +0700

    component

---
+++ src/components/MyComponents.js
@@ -0,0 +1,22 @@
+// class component
+// function component
+
+import React from "react";
+
+
+class MyComponent extends React.Component {
+
+    //JSX
+    render() {
+        return (
+            <div>
+                my fisrt component
+
+                {Math.random()}
+            </div>
+        )
+
+    }
+}
+
+export default MyComponent;
