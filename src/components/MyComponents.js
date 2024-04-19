6f3278ca1841fefcb5a0fb63f98d416c81582d9d
bobeo13082003 <phamichbo13082003@gmail.com>
2024-04-19 10:40:16 +0700

    even and state

--- src/components/MyComponents.js
+++ src/components/MyComponents.js
@@ -6,13 +6,32 @@ import React from "react";
 
 class MyComponent extends React.Component {
 
+    state = {
+        name: 'Bo Beo',
+        address: 'Bac Ninh',
+        age: 21
+    }
+
+
+    handleClick(even) {
+        // console.log('>> Click Me ');
+        // console.log(even);
+    }
+
+
+    handleOnMouseOver(even) {
+        console.log(even);
+    }
+
     //JSX
     render() {
         return (
             <div>
-                my fisrt component
+                My name is:
 
-                {Math.random()}
+                {this.state.name} and I'm from {this.state.address}
+                <button handleOnMouseOver={this.handleOnMouseOver}>Hover Me</button>
+
             </div>
         )
 
