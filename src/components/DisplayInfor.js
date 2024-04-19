2926cbdf04fa6b97d48f29eed6b7578bfa1b4ff4
bobeo13082003 <phamichbo13082003@gmail.com>
2024-04-19 22:37:50 +0700

    if else in react

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -2,13 +2,51 @@ import React from "react";
 import UserInfor from "./UserInfor";
 
 class DisplayInfor extends React.Component {
+    state = {
+        isShowListUser: true
+
+    }
+
+    handleShowHide = () => {
+        this.setState({
+            isShowListUser: !this.state.isShowListUser
+        })
+    }
+
     render() {
+
+
+
         // Distructuring array/object
-        const { age, name } = this.props;
+        const { listUsers } = this.props;
+        // console.log(listUsers);
+        // console.table(listUsers);
         return (
             <div>
-                <div>My Name {name}</div>
-                <div>My age {age}</div>
+                <div>
+                    <span onClick={() => { this.handleShowHide() }}>
+                        {this.state.isShowListUser === true ? "Hide list user" : "Show list user"}
+                    </span>
+                </div>
+                {this.state.isShowListUser &&
+                    <div>
+                        {listUsers.map((user) => {
+
+                            return (
+                                <div key={user.id} className={+user.age >= 30 ? "green" : "red"}>
+                                    <div>My name {user.name}</div>
+                                    <div>My age {user.age}</div>
+                                    <hr />
+                                </div>
+                            )
+
+
+
+                        })}
+                        {/* <div>My Name {name}</div>
+                <div>My age {age}</div> */}
+                    </div>
+                }
             </div>
         )
     }
--- src/components/MyComponents.js
+++ src/components/MyComponents.js
@@ -7,13 +7,22 @@ import DisplayInfor from "./DisplayInfor";
 
 
 class MyComponent extends React.Component {
+    state = {
+        listUsers: [
+            { id: 1, name: 'bo', age: '30' },
+            { id: 2, name: 'Binh', age: '24' },
+            { id: 3, name: 'Tuan', age: '56' }
+        ]
+    }
+
     //JSX
     render() {
+        //DRY: don't repeat youseft
         return (
             <div>
                 <UserInfor />
                 <br />
-                <DisplayInfor name="Bo Beo" age="25" />
+                <DisplayInfor listUsers={this.state.listUsers} />
             </div>
         )
 
